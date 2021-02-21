44f7ac2b9f627a833317c76434c4884e6be6b95c
jonhayjr <jonhayjr@gmail.com>
2021-02-21 09:04:56 -0800

    Added JS HTML for alert banner.  Added styling for banner

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const alertBanner = document.querySelector('#alert');
+
+//Create the HTML for the banner   
+alertBanner.innerHTML =
+`
+<div class="alert-banner">
+    <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete.</p>
+    <p class="alert-banner-close">x</p>
+</div>
+`

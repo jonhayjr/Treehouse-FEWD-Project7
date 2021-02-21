90dc30e234837559d7aa75318429f8c5e31e3d9f
jonhayjr <jonhayjr@gmail.com>
2021-02-21 10:35:33 -0800

    Added message form field validation

--- js/app.js
+++ js/app.js
@@ -118,3 +118,21 @@ let mobileChart = new Chart(mobileCanvas, {
     data: mobileData,
     options: mobileOptions
     });
+
+//Messaging Section   
+const user = document.querySelector('#userField');
+const message = document.querySelector('#messageField');
+const send = document.querySelector('#send');
+
+send.addEventListener('click', () => {
+    // ensure user and message fields are filled out
+    if (user.value === "" && message.value === "") {
+    alert("Please fill out user and message fields before sending");
+    } else if (user.value === "" ) {
+    alert("Please fill out user field before sending");
+    } else if (message.value === "" ) {
+    alert("Please fill out message field before sending");
+    } else {
+    alert(`Message successfully sent to: ${user.value}`);
+    }
+    });

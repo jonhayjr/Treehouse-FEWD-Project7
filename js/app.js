60a7c728d870b07c940a9260b3a1268d11a9aa91
jonhayjr <jonhayjr@gmail.com>
2021-02-21 09:58:52 -0800

    Added traffic chart js and updated formatting

--- js/app.js
+++ js/app.js
@@ -8,3 +8,48 @@ alertBanner.innerHTML =
     <p class="alert-banner-close">x</p>
 </div>
 `
+
+//Alert Banner Event Listener
+alertBanner.addEventListener('click', e => {
+    const element = e.target;
+    if (element.classList.contains("alert-banner-close")) {
+        alertBanner.style.display = "none"
+    }
+});
+
+//Traffic Chart  
+const trafficCanvas = document.querySelector('#traffic-chart');
+
+let trafficData = {
+    labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
+    "4-10", "11-17", "18-24", "25-31"],
+    datasets: [{
+    data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500,
+    2500],
+    backgroundColor: 'rgba(116, 119, 191, .3)',
+    borderWidth: 1,
+    }]
+    };
+
+let trafficOptions = {
+    aspectRatio: 2.5,
+    animation: {
+    duration: 0
+    },
+    scales: {
+    yAxes: [{
+    ticks: {
+    beginAtZero:true
+    }
+    }]
+    },
+    legend : {
+    display: false
+    }
+    };
+
+let trafficChart = new Chart(trafficCanvas, {
+    type: 'line',
+    data: trafficData,
+    options: trafficOptions
+    });
